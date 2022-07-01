dc00085b19ddbd73a791f2e439453d66133c79c7
Scott <ScottAdelman97@gmail.com>
2022-07-01 10:19:46 -0600

    demo

---
+++ Variable_Demo.cs
@@ -0,0 +1,25 @@
+﻿        String myDogsName;
+        myDogsName = "Loki";
+        Console.WriteLine(myDogsName);
+
+        int myDogsAge;
+        myDogsAge = 2;
+        Console.WriteLine(myDogsAge);
+
+        char randomLetter;
+        randomLetter = 'q';
+        Console.WriteLine(randomLetter);
+
+        bool iHaveADog;
+        iHaveADog = true;
+        Console.WriteLine(iHaveADog);
+
+        double itemPrice;
+        itemPrice = 3.99;
+        Console.WriteLine(itemPrice);
+
+        Decimal battingAverage;
+        battingAverage = 3;
+        Console.WriteLine(battingAverage);
+
+        Console.WriteLine("My Dog " + myDogsName + " " + "is" + " " + myDogsAge + " " + "years old!");
